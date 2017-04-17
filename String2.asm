d1ce486ffbc15e4187cdd7fdf1903ee8213834ee
walla92 <cheeseedoodle@yahoo.com>
2017-04-16 19:01:28 -0700

    indexOf2,indexOf3,lastIndexOf3 checks added
    indexOf2 added check if index > string length
    indexOf3 added check if substring length > string length
    lastIndexOf3 added check if substring length > string length

--- String2.asm
+++ String2.asm
@@ -29,7 +29,7 @@ COMMENT %			;terminating symbol for the block is
 *****************************************************************************************
 * Name: String_length									*
 * Purpose:										*
-*		Determine the number of characters in a string				*
+*	Determine the number of characters in a string (not including the null)		*
 *											*
 *   @param  lpString1:dword  								*
 *   @return length:dword  the length of a the string	                                *
@@ -61,7 +61,8 @@ COMMENT %
 *****************************************************************************************
 * Name: String_indexOf1									*
 * Purpose:										*
-*		Returns the position in the string of the char value(if found)		*
+*	Returns the position of a given character in a given string(if found), if not	*
+*	returns -1.									*
 *											*
 *   @param  lpString1:dword char:byte							*
 *   @return lastVal:dword  the position of the char value in the string(if found)	*
@@ -72,21 +73,21 @@ String_indexOf1 proc Near32
 	push ebx					;preserve used registers
 	push esi
 	
-	mov ebx,[ebp+8]		;string
-	mov ax, [ebp+12]	;character
-	mov esi,0			;start searching from the start
+	mov ebx,[ebp+8]		;ebx -> string
+	mov ax, [ebp+12]	;ax -> character
+	mov esi,0		;start searching from the begining of the string
 stLoop:
 	cmp byte ptr [ebx+esi], 0	;checks for the end of the string
 	je charNotFound
 	cmp byte ptr [ebx+esi], al	;compares the char in the string with the search char
 	je charFound	;char found
-	inc esi			;char not found
+	inc esi		;char not found
 	jmp stLoop
 charFound:
 	mov eax,esi		;stores index into eax
 	jmp exit
 charNotFound:	
-	mov eax, -1
+	mov eax, -1		;string ended without finding the char, return -1
 exit:
 	pop esi			;restore preserved registers
 	pop ebx
@@ -98,8 +99,9 @@ COMMENT %
 *****************************************************************************************
 * Name: String_indexOf2									*
 * Purpose:										*
-*		Returns the position in the string of the char value(if found), starting*
-*	from a specified index								*
+*	Returns the position of a given character in a given string(if found) starting  *
+*from a given point in the string. If the character is not found or the given 		*
+*position(index) is larger then the length of the string then returns -1.		*
 *											*
 *   @param  lpString1:dword, char:byte, int:dword					*
 *   @return firstVal:dword  the position of the char value in the string(if found)	*
@@ -110,21 +112,23 @@ String_indexOf2 proc Near32
 	push ebx					;preserve used registers
 	push esi
 	
-	mov ebx,[ebp+8]		;string
+	mov ebx,[ebp+8]		;ebx -> string
 	
-	push ebx			;pushe string, for string_length
+	push ebx			;pass string for string_length
 	call String_length	;get the length of the string (value in eax)
 	add esp,4			;fix the stack
 	
-	mov ax, [ebp+12]	;character
-	mov esi, [ebp+14]	;index
-	cmp esi, eax		;checks if given index is greater than the length of the string
-	jg charNotFound		;if index > length of string, exit
+	mov esi, [ebp+14]	;esi -> index
+	
+	cmp esi, eax		;check if the index is within the strings length
+	jg charNotFound		;exits if index > length
+	
+	mov ax, [ebp+12]	;ax -> character
 stLoop:
 	cmp byte ptr [ebx+esi], 0	;checks for end of string	
 	je charNotFound
-	cmp byte ptr [ebx+esi], al	;compares char in string with the search char
-	je charFound				;char found
+	cmp byte ptr [ebx+esi], al	;compares char in string with char its looking for
+	je charFound			;char found
 	inc esi
 	jmp stLoop
 charFound:
@@ -143,7 +147,9 @@ COMMENT %
 *****************************************************************************************
 * Name: String_indexOf3									*
 * Purpose:										*
-*		Returns the position of the first occurence of a substring a string 	*
+*	Returns the position of the first occurence of a given substring(string2) in a	*
+*given string(string1). Returns -1 if either	the substring(string2) is larger than   *
+*the main string(string1) or the substring wasn't found.				*
 *											*
 *   @param  lpString1:dword, lpSubStr:dword						*
 *   @return firstVal:dword  the position of the string					*
@@ -156,9 +162,20 @@ String_indexOf3 proc Near32
 	push ecx
 	push edx
 	
-	mov ebx,[ebp+8]		;string
-	mov ecx,[ebp+12]	;substring
-	mov esi,0
+	mov ebx,[ebp+8]		;ebx -> string1 (string)
+	mov ecx,[ebp+12]	;ecx -> string2 (substring)
+	
+	push ebx		;pass string1 for string_length
+	call String_length	;get the length of string1
+	add esp,4
+	mov dVal1,eax
+	push ecx		;pass string2 for string_length
+	call String_length	;get the length of string2(substring)
+	add esp,4	
+	cmp eax,dVal1		;check if string2 > string1 (substring is longer then main string)
+	jg strEnd		;substring larger than main string, return -1 and exit
+	
+	mov esi,0	
 	
 stLoop:
 	mov edx,0
@@ -166,7 +183,7 @@ stLoop:
 subLoop:
 
 	cmp byte ptr [ecx+edx],0	;checks for end of substring
-	je found		;if substring ends exit
+	je found		;if substring ends(then string2 found within string1), exit
 
 	cmp byte ptr [ebx+esi],0	;check end of string
 	je strEnd		;end of string(substring not found), exit
@@ -195,7 +212,8 @@ COMMENT %
 *****************************************************************************************
 * Name: String_toLowerCase								*			
 * Purpose:										*						
-*		Replaces any uppercase letter in a string with its lowercase verssion	*	    
+*	Replaces any uppercase letter in a string with its lowercase verssion.		*
+*Changes are made directly to the string (nothing is returned in a register)		*	    
 *											*
 *   @param  lpString1:dword								*						
 *   @return lpString1:dword								*
@@ -207,11 +225,11 @@ String_toLowerCase proc Near32
 	push esi
 	push eax
 	
-	mov ebx,[ebp+8]		;string
-	mov esi,0			;index for navigating the string	
+	mov ebx,[ebp+8]		;ebx -> string
+	mov esi,0		;index for navigating the string	
 stLoop:
 	cmp byte ptr[ebx+esi],0	;checks for the end of a string
-	je finished				;if yes, exit
+	je finished		;if yes, exit
 	
 	cmp byte ptr[ebx+esi],65	;checks lower bound for uppercase chars
 	jl reLoop
@@ -219,7 +237,7 @@ stLoop:
 	jg reLoop
 	
 	mov al,byte ptr[ebx+esi]	;gets the value of the char in the string
-	add al,32					;sets it to its lowercase value
+	add al,32			;sets it to its lowercase value
 	mov byte ptr[ebx+esi],al	;stores the lowercase value back in the string
 reLoop:
 	inc esi					;if not, go to the next character
@@ -237,7 +255,8 @@ COMMENT %
 *****************************************************************************************
 * Name: String_toUpperCase								*								
 * Purpose:										*										
-*		Replaces any lowercase letter in a string with its uppercase verssion	*	    
+*		Replaces any lowercase letter in a string with its uppercase verssion	*
+*Changes are made directly to the string (nothing is returned in a register)		*
 *											*											*
 *   @param  lpString1:dword								*								
 *   @return lpString1:dword								*								
@@ -249,11 +268,11 @@ String_toUpperCase proc Near32
 	push esi
 	push eax
 	
-	mov ebx,[ebp+8]		;string
-	mov esi,0			;index for navigating the string	
+	mov ebx,[ebp+8]		;ebx -> string
+	mov esi,0		;index for navigating the string	
 stLoop:
 	cmp byte ptr[ebx+esi],0	;checks for the end of a string
-	je finished				;if yes, exit
+	je finished		;if yes, exit
 	
 	cmp byte ptr[ebx+esi],97	;checks lower bound for lowercase chars
 	jl reLoop
@@ -261,7 +280,7 @@ stLoop:
 	jg reLoop
 	
 	mov al,byte ptr[ebx+esi]	;gets the value of the char in the string
-	sub al,32					;sets it to its uppercase value
+	sub al,32			;sets it to its uppercase value
 	mov byte ptr[ebx+esi],al	;stores the uppercase value back in the string
 reLoop:
 	inc esi					;if not, go to the next character
@@ -279,7 +298,8 @@ COMMENT %
 *****************************************************************************************
 * Name: String_replace									*								
 * Purpose:										*										
-*		Replaces all oldChars in string with NewChars				*					
+*		Replaces all oldChars in string with NewChars				*
+*Changes are made directly to the string (nothing is returned in a register)		*
 *											*											*
 *   @param  lpString1:dword oldChar:byte newChar:byte					*				
 *   @return lpString1:dword								*								
@@ -292,16 +312,16 @@ String_replace proc Near32
 	push ecx
 	push eax
 	
-	mov ebx,[ebp+8]		;string
-	mov ax, [ebp+12]	;old char
-	mov cx, [ebp+14]	;new char
-	mov esi,0					;esi indexes into the strings	
+	mov ebx,[ebp+8]		;ebx -> string
+	mov ax, [ebp+12]	;ax -> old char (to be replaced by the new char)
+	mov cx, [ebp+14]	;cx -> new char (to replace the old char)
+	mov esi,0		;index for nagivating through the string, starting from the begining
 stLoop:
-	cmp byte ptr[ebx+esi],0	;checks if it has reached the end of a string
-	je finished				;if yes, exit
+	cmp byte ptr[ebx+esi],0		;checks if it has reached the end of a string
+	je finished			;if yes, exit
 	cmp byte ptr[ebx+esi], al	;check the char in the string
-	jne skip
-	mov byte ptr[ebx+esi], cl	;replaces the char in the string
+	jne skip			;not found, go to next char in string
+	mov byte ptr[ebx+esi], cl	;found, replaces the char in the string
 skip:
 	inc esi					;if not, go to the next character
 	jmp stLoop				;loops until you hit a NULL character
@@ -331,21 +351,21 @@ String_lastIndexOf1 proc Near32
 	push ebx					;preserve used registers
 	push esi
 	
-	mov ebx,[ebp+8]		;string
+	mov ebx,[ebp+8]		;ebx -> string
 	
-	push ebx			;push the string
+	push ebx		;pass the string for string_length
 	call String_Length	;get the strings length
-	add esp,4			;fix the stack
-	mov esi,eax			;store string length into esi
+	add esp,4		;fix the stack
+	mov esi,eax		;store string length into esi
 	
-	mov ax, [ebp+12]	;character
+	mov ax, [ebp+12]	;ax -> character
 stLoop:
-	cmp esi,-1		;checks if it has gone through the entire loop(nagivates through string backwards)
-	je notFound
+	cmp esi,-1		;checks if it has gone through the entire string (nagivates from end to begining of string)
+	je notFound		;char not found in string
 	cmp byte ptr [ebx+esi], al	;checks for the char in the string
-	je charFound	;char found
-	dec esi			;char not found
-	jmp stLoop	;loops
+	je charFound			;char found
+	dec esi				;char not found
+	jmp stLoop			;loops
 charFound:
 	mov eax,esi		;stores index into eax
 	jmp exit
@@ -374,25 +394,26 @@ String_lastIndexOf2 proc Near32
 	push ebx					;preserve used registers
 	push esi
 	
-	mov ebx,[ebp+8]		;string
+	mov ebx,[ebp+8]		;ebx -> string
 	
-	push ebx			;pushes the string, for string_length
+	push ebx		;pushes the string, for string_length
 	call String_length	;get the length of the string (value in eax)
-	add esp,4
-	mov ecx,eax
+	add esp,4		;fix stack
+	mov ecx,eax		;store length of string in ecx
 	
-	mov ax, [ebp+12]	;character
-	mov esi, [ebp+14]	;index
+	mov esi, [ebp+14]	;esi -> index
 	
 	cmp esi, ecx		;checks if given index is greater than the length of the string
 	jg charNotFound		;if index > length of string, exit
+
+	mov ax, [ebp+12]	;ax -> character
 stLoop:
-	cmp esi, -1		;checks for end of string	
+	cmp esi, -1		;checks if it has gone through the entire string (nagivates from end to begining of string)	
 	je charNotFound
 	cmp byte ptr [ebx+esi], al	;compares char in string with the search char
-	je charFound	;char found
-	dec esi			;char not found
-	jmp stLoop	;loops
+	je charFound			;char found
+	dec esi				;char not found
+	jmp stLoop			;loops
 charFound:
 	mov eax,esi	;stores index of char
 	jmp exit
@@ -422,35 +443,46 @@ String_lastIndexOf3 proc Near32
 	push ecx
 	push edx
 	
-	mov ebx,[ebp+8]		;string
-	mov ecx,[ebp+12]	;substring
-	mov esi,0
-	mov dVal2,-1	;if substring found, updates dVal2
+	mov dVal2,-1	;store start of substring in dVal2(initaly -1, updated if substring found)
 	
+	mov ebx,[ebp+8]		;ebx - >string
+	mov ecx,[ebp+12]	;ecx -> substring
+	
+	push ebx			;passes string for string_length
+	call String_length		;get strings length
+	add esp,4
+	mov edx,eax			;stores strings length into edx
+	push ecx			;passes substring for string_length
+	call String_length		;get substrings length
+	add esp,4
+	cmp eax, edx			;compares substrings length with strings length
+	jg exit				;if substring longer than string exit
+	
+	mov esi,0	;nagivate main string from start
 stLoop:
-	mov edx,0
-	mov dVal1, esi ;store index of 1st substr val in dVal1
+	mov edx,0	;index for substring
+	mov dVal1, esi ;store current index of main string in dVal1(start of substring in main string)
 subLoop:
 	mov eax,0
 	
 	cmp byte ptr [ecx+edx],0	;checks for end of substring(found in string)
-	je found		;if substring ends store position
+	je found			;if substring ends store position
 
 	cmp byte ptr [ebx+esi],0	;check end of string
-	je exit		;if end of main string exit
+	je exit				;if end of main string exit
 	
 	mov al, byte ptr[ebx+esi]	;store value at index of string
 	inc esi	
 	cmp byte ptr [ecx+edx],al	;compare value in substring with stroed value form string
-	jne stLoop	;not equal, loop at stLoop
+	jne stLoop			;not equal, loop at stLoop
 	inc edx
-	jmp subLoop	;equal, loop at subLoop
+	jmp subLoop			;equal, loop at subLoop
 found:
-	mov eax, dVal1	;substr found, store 1st index into dVal2 
+	mov eax, dVal1	;substr found, store 1st index of the substring(dVal1) into dVal2 
 	mov dVal2,eax
 	jmp stLoop
 exit:
-	mov eax,dVal2
+	mov eax,dVal2	;store index value (-1 if not found or substring > string length), into eax
 
 	pop edx
 	pop ecx
@@ -465,7 +497,7 @@ COMMENT %
 * Name: String_concat									*								
 * Purpose:										*										
 *	Takes two strings (string1, string2) and addds the second string to the end 	*
-*	of the first									*									
+*of the first. Returns the address of the new string in EAX.				*									
 *											*											*
 *   @param  lpString1:dword, lpString2:dword						*					
 *   @return lpNewString:dword (in eax)							*				  		
